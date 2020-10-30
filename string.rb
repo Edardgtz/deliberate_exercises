1e4f74eec58e06a4882f999859b77146197b0ea8
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-10-30 10:47:55 -0500

    Completed exercise 4 of the deliberate practice sheet.

--- string.rb
+++ string.rb
@@ -1,25 +1,39 @@
 # 1
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
 # first_name = "Eduardo"
 # last_name = "Gutierrez"
 # puts first_name + " " + last_name
 
-# 2
+# 2 
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
 # first_name = "Eduardo"
 # last_name = "Gutierrez"
 # puts "#{first_name} #{last_name}"
 
-# 3
-puts "Enter a word:"
-x = true
-while x
-  user_input = gets.chomp.downcase
-  x = false
-  if user_input == "marco"
-    puts "polo"
-    x = false
-  else 
-    puts "Wrong word. Enter another:"
-    x = true
-  end
-end
+# 3  
+# Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+# puts "Enter a word:"
+# x = true
+# while x
+#   user_input = gets.chomp.downcase
+#   x = false
+#   if user_input == "marco"
+#     puts "polo"
+#     x = false
+#   else 
+#     puts "Wrong word. Enter another:"
+#     x = true
+#   end
+# end
+
+# 4 
+# Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+x = "red"
+y = "green"
+z = "teal"
+puts "I saw a " + x + " bird on a " + y + " car with " + z + " rims."
 
