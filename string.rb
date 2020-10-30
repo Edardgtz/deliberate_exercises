db33a83e07a81cbeefdd4690aedd30c85cfb9369
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-10-30 10:20:21 -0500

    added exercise 3 to stringh.rb from the deliberate practice sheet

--- string.rb
+++ string.rb
@@ -1,4 +1,25 @@
 # 1
-first_name = "Eduardo"
-last_name = "Gutierrez"
-puts "#{first_name} #{last_name}"
+# first_name = "Eduardo"
+# last_name = "Gutierrez"
+# puts first_name + " " + last_name
+
+# 2
+# first_name = "Eduardo"
+# last_name = "Gutierrez"
+# puts "#{first_name} #{last_name}"
+
+# 3
+puts "Enter a word:"
+x = true
+while x
+  user_input = gets.chomp.downcase
+  x = false
+  if user_input == "marco"
+    puts "polo"
+    x = false
+  else 
+    puts "Wrong word. Enter another:"
+    x = true
+  end
+end
+
