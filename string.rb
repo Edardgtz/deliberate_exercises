f9441691e0aa5052142837e48499758c569a4334
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-01 08:15:30 -0600

    Completed exercise 5 of the deliberate practice sheet.

--- string.rb
+++ string.rb
@@ -32,8 +32,15 @@
 # 4 
 # Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
+# x = "red"
+# y = "green"
+# z = "teal"
+# puts "I saw a " + x + " bird on a " + y + " car with " + z + " rims."
+
+# 5
+# Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
 x = "red"
 y = "green"
-z = "teal"
-puts "I saw a " + x + " bird on a " + y + " car with " + z + " rims."
-
+z = "yellow"
+puts "I saw a #{x} bird on a #{y} car with #{z} rims."
