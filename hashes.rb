12233be60ea21b7cb583ccf803dc4a1cc8125568
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-03 16:11:14 -0600

    Completed exercise 2 of skill sheet

--- hashes.rb
+++ hashes.rb
@@ -1,5 +1,9 @@
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
-person1 = {first_name: "Eduardo", last_name: "Gutierrez", email: "eduardo.gutierrez@gamil.com"}
-puts person1[:first_name]
-puts person1[:last_name]
-puts person1[:email]
+# person1 = {first_name: "Eduardo", last_name: "Gutierrez", email: "eduardo.gutierrez@gamil.com"}
+# puts person1[:first_name]
+# puts person1[:last_name]
+# puts person1[:email]
+
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+people = [{first_name: "Bobby", last_name: "Miller"}, {first_name: "Constancio", last_name: "Latzio"}, {first_name: "Bishop", last_name: "Jones"}]
+puts people[0]
