5e0ef39ae56e7a4d55e524ea718598710e353c6b
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-04 12:29:04 -0600

    Completed exercise 3 of the deliberate exercise sheet

--- hashes.rb
+++ hashes.rb
@@ -5,5 +5,10 @@
 # puts person1[:email]
 
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
-people = [{first_name: "Bobby", last_name: "Miller"}, {first_name: "Constancio", last_name: "Latzio"}, {first_name: "Bishop", last_name: "Jones"}]
-puts people[0]
+# people = [{first_name: "Bobby", last_name: "Miller"}, {first_name: "Constancio", last_name: "Latzio"}, {first_name: "Bishop", last_name: "Jones"}]
+# puts people[0]
+
+# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+menu = {hambuger: "7.99", fries: "2.49", milkshake: "3.49"}
+menu[:onion_rings] = "2.99"
+p menu
