af7dae9e50f8674bf71b1ba677787f4d48e4ed60
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-06 10:30:14 -0600

    Completed exercise 2 of deliberate preactice sheet

--- loops.rb
+++ loops.rb
@@ -1,6 +1,13 @@
 # 1. Write a while loop to print the numbers 1 through 10.
-i = 1
-while i <= 10
-  p i
+# i = 1
+# while i <= 10
+#   p i
+#   i += 1
+# end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+i = 0
+while i <= 4
+  p "hello"
   i += 1
 end
