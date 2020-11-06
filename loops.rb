59216ec8258e7bfa1e54f6c4696452a8cc1577ac
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-06 10:40:30 -0600

    Completed exercise 4 of deliberate preactice sheet

--- loops.rb
+++ loops.rb
@@ -13,14 +13,21 @@
 # end
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
-x = true
-while x
-  puts "Enter a word: "
-  user_input = gets.chomp.downcase
-  if user_input == 'stop'
-    puts "OK! Fine I'll stop. The temper on this one."
-    x = false
-  else
-    puts "What's the magic word?"
-  end
+# x = true
+# while x
+#   puts "Enter a word: "
+#   user_input = gets.chomp.downcase
+#   if user_input == 'stop'
+#     puts "OK! Fine I'll stop. The temper on this one."
+#     x = false
+#   else
+#     puts "What's the magic word?"
+#   end
+# end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+i = 0
+while i <= 100
+  p i
+  i += 5
 end
