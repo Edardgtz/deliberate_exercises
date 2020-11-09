956008b29e3cf5ec84f5b6d302ca9873c3cc88f0
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-09 15:18:36 -0600

    completed ex. 2 of arrays practice sheet

--- arrays.rb
+++ arrays.rb
@@ -1,11 +1,22 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
-numbers = [2, 32, 80, 18, 12, 3]
-under_twenty = []
-numbers.each do |number|
-  if number < 20
-    under_twenty << number
+# numbers = [2, 32, 80, 18, 12, 3]
+# under_twenty = []
+# numbers.each do |number|
+#   if number < 20
+#     under_twenty << number
+#   end
+# end
+# p under_twenty
+
+#  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+#     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+strings = ["winner", "winner", "chicken", "dinner"]
+starts_with_w = []
+strings.each do |string|
+  if string[0] == 'w'
+    starts_with_w << string
   end
 end
-p under_twenty
-
+p starts_with_w
