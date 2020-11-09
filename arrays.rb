1fb9f5e4983d00ec55f46d8637a2aca577f52e1f
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-09 15:22:47 -0600

    completed ex. 3 of arrays practice sheet

--- arrays.rb
+++ arrays.rb
@@ -12,11 +12,23 @@
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
-strings = ["winner", "winner", "chicken", "dinner"]
-starts_with_w = []
-strings.each do |string|
-  if string[0] == 'w'
-    starts_with_w << string
+# strings = ["winner", "winner", "chicken", "dinner"]
+# starts_with_w = []
+# strings.each do |string|
+#   if string[0] == 'w'
+#     starts_with_w << string
+#   end
+# end
+# p starts_with_w
+
+#  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+items_over_five = []
+items.each do |item|
+  if item[:price] > 5
+    items_over_five << item
   end
 end
-p starts_with_w
+
+p items_over_five
