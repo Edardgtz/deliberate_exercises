27c91608eb805ffd554ba3c4612925d8f88c2027
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-16 13:12:29 -0600

    Completed exercise 3  of nested arrays. week 4

--- nested_arrays.rb
+++ nested_arrays.rb
@@ -14,13 +14,29 @@
 #  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
 #     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
 
-letters_one = ["a", "b", "c"]
-letters_two = ["d", "e", "f", "g"]
-letters_combined = []
-letters_one.each do |letter_a|
-  letters_two.each do |letter_b|
-    letters_combined << letter_a + letter_b
+# letters_one = ["a", "b", "c"]
+# letters_two = ["d", "e", "f", "g"]
+# letters_combined = []
+# letters_one.each do |letter_a|
+#   letters_two.each do |letter_b|
+#     letters_combined << letter_a + letter_b
+#   end
+# end
+
+# p letters_combined
+
+#  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
+#     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
+
+letters = ["a", "b", "c", "d"]
+letter_combos = []
+letters.each do |letter_a|
+  letters.each do |letter_b|
+    if letter_a == letter_b
+    else
+      letter_combos << letter_a + letter_b 
+    end
   end
 end
 
-p letters_combined
+p letter_combos
