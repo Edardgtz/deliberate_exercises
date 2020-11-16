e512b44542cbf203136a14e49afb6713cc2b5896
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-16 17:58:52 -0600

    Completed exercise 5  of nested arrays. week 4

--- nested_arrays.rb
+++ nested_arrays.rb
@@ -44,18 +44,38 @@
 #  4. Use a nested loop to find the largest product of any two different numbers within a given array.
 #     For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
 
-def find_product(user_input)
-  @user_input = user_input
-  max_product = 0
-  @user_input.each do |number_a|
-    @user_input.each do |number_b|
-      product = number_a * number_b
-      if number_a != number_b && max_product < product
-        max_product = number_a * number_b        
+# def find_product(user_input)
+#   @user_input = user_input
+#   max_product = 0
+#   @user_input.each do |number_a|
+#     @user_input.each do |number_b|
+#       product = number_a * number_b
+#       if number_a != number_b && max_product < product
+#         max_product = number_a * number_b        
+#       end
+#     end
+#   end
+#   p max_product
+# end
+
+# find_product([5, -2, 1, -9, -7, 2, 6])
+
+#  5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
+#     For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
+
+def find_sum(numbers)
+  sum = 0
+  total_sum = 0
+  numbers.each do |pair|
+    pair.each do |number|
+      if sum != number
+        sum = sum + number
+      else
+        
       end
     end
   end
-  p max_product
+  p sum
 end
 
-find_product([5, -2, 1, -9, -7, 2, 6])
+find_sum([[1, 3], [8, 9], [2, 16]])
