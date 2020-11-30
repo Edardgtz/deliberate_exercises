a86f6cf137b841cc64af7d5989878c9063f05d66
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-30 15:18:32 -0600

    Completed exe. 3 of week 7 deliberate practice.

--- converting_data.rb
+++ converting_data.rb
@@ -8,11 +8,18 @@
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
 
-array_of_hashes = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
-@new_hash = Hash.new
-array_of_hashes.each do |hash|
-  id = hash.fetch(:id)
-  @new_hash[id] = hash
-end
+# array_of_hashes = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
+# @new_hash = Hash.new
+# array_of_hashes.each do |hash|
+#   id = hash.fetch(:id)
+#   @new_hash[id] = hash
+# end
 
-puts @new_hash
+# puts @new_hash
+
+#  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
+#     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+
+string = "bookkeeper"
+number_of_times_characters_appear = string.each_char.tally
+p number_of_times_characters_appear
